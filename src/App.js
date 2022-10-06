adf63596346e879dcee1049e9e6e75ce33ce3687
Nahid-07 <nahidasif1998@gmail.com>
2022-10-06 13:06:21 +0600

    Pro Reader project completely done with reat router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
-import React from 'react'
+import React from 'react';
+import Home from './components/Home';
+import About from './components/About'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Books from './components/Books';
+import BookDetails from './components/BookDetails'
+import Root from './components/Root';
+
+const router = createBrowserRouter([
+  {
+    path:'/', element:<Root></Root>, children:[
+      {
+        path:'/', element:<Home></Home>
+      },
+      {
+        path:'/home', element:<Home></Home>
+      },
+      {
+        path:'/about', element:<About></About>
+      },
+      {
+        path:'/books',element:<Books></Books>,
+        loader:()=> fetch('https://api.itbook.store/1.0/new')
+      },
+      {
+        path: '/book/:id',
+        element:<BookDetails></BookDetails>,
+        loader:({params})=>fetch(`https://api.itbook.store/1.0/books/${params.id}`)
+      }
+    ]
+  }
+])
 
 function App() {
-  return <h1> Welcome to proReader</h1>
+  return (
+    <div>
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  )
 }
 
 export default App
